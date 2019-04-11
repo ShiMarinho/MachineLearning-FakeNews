1f1ebbf7b594aacc705b414872b073b8c8fd8df9
adeloaleman <adeloaleman@gmail.com>
2019-04-12 00:38:04 +0100

    commit

--- fakeNewsBackEnd/fakeNewsChallengeDataset/fakeNewsChallenge-XGBoost.R
+++ fakeNewsBackEnd/fakeNewsChallengeDataset/fakeNewsChallenge-XGBoost.R
@@ -1,5 +1,5 @@
-# https://gist.github.com/dkincaid/87f0fbeb912cf23816c340b4fbe30baa
-# https://community.rstudio.com/t/document-term-matrix-in-xgboost-classifier/10043/12
+# https://cran.r-project.org/web/packages/xgboost/xgboost.pdf
+# https://stackoverflow.com/questions/1299871/how-to-join-merge-data-frames-inner-outer-left-right
 
 library(xgboost)
 library(caret)
@@ -7,6 +7,7 @@ library(tm)
 library(text2vec)
 library(pdp)
 library(Matrix)
+library(dplyr)
 
 
 
@@ -14,9 +15,11 @@ library(Matrix)
 # =============================================================================== =
 #  Loading the dataset ----
 # =============================================================================== =
-train_dataset <- read.csv("train.csv")
-# train_dataset <- read.csv("/home/adelo/gofaaas/r_prog/4-Fake_news_dataset_using_RTextTools_and_XGBoost/train.csv")
-# train_dataset <- read.csv("/home/adelo/1-system/1-disco_local/1-mis_archivos/1-pe/1-ciencia/1-computacion/2-data_analysis-machine_learning/gofaaaz-machine_learning/r_prog/4-Fake_news_dataset_using_RTextTools_and_XGBoost/train.csv",  nrows = 100)
+train_stances <- read.csv("/home/adelo/gofaaas/r_prog/FakeNewsChallenge/train_stances.csv")
+train_bodies  <- read.csv("/home/adelo/gofaaas/r_prog/FakeNewsChallenge/train_bodies.csv")
+# 
+# train_stances <- read.csv("/home/adelo/1-system/desktop/it_cct/5-Applied_Technology_Group_Project/gofaaaz-machine_learning/r_prog/6-FakeNewsChallenge/train_stances.csv",  nrows = 1000)
+# train_bodies  <- read.csv("/home/adelo/1-system/desktop/it_cct/5-Applied_Technology_Group_Project/gofaaaz-machine_learning/r_prog/6-FakeNewsChallenge/train_bodies.csv",  nrows = 1000)
 
 
 
@@ -24,7 +27,10 @@ train_dataset <- read.csv("train.csv")
 # =============================================================================== =
 #  Splitting the data into Train and Test data ----
 # =============================================================================== =
-dataset <- train_dataset
+dataset <- merge(train_stances, train_bodies)
+dataset <- dataset[c("Body.ID", "articleBody", "Headline", "Stance")]
+
+dataset = dataset[1:1000,]
 
 set.seed(16102016)                             # To fix the sample 
 
@@ -46,19 +52,26 @@ data = rbind(train,test)
 # =============================================================================== =
 #  Text processing ----
 # =============================================================================== =
-data$text = tolower(data$text)      # Convert to lower case
-text = data$text                    # Taking just the text column
-text = removePunctuation(text)      # Remove punctuation marks
-text = removeNumbers(text)          # Remove numbers
-text = stripWhitespace(text)        # Remove blank space
+
+# Text
+text <- paste(data$articleBody, data$Headline)
+
+text = tolower(text)            # Convert to lower case
+text = removePunctuation(text)  # Remove punctuation marks
+text = removeNumbers(text)      # Remove numbers
+text = stripWhitespace(text)    # Remove blank space
 
 text_train = text[1:nrow(train)]
 text_test  = text[(nrow(train)+1):nrow(data)]
 
 
-# Coded labels
-labels_train = train$label
-labels_test  = test$label
+# Labels
+lb <- as.numeric(data$Stance) - 1
+
+num_class <- 4
+
+labels_train = lb[1:nrow(train)]
+labels_test  = lb[(nrow(train)+1):nrow(data)]
 
 
 
@@ -88,7 +101,6 @@ vocab_test <- create_vocabulary(itoken(text_test,
 # =============================================================================== =
 #  Build a document-term matrix using the tokenized review text. This returns a dgCMatrix object ----
 # =============================================================================== =
-
 
 # ---------------------------------------------------------------- -
 #   * Document-term matrix for the train data ----
@@ -121,15 +133,11 @@ train_matrix <- xgb.DMatrix(dtm_train, label = labels_train)
 # =============================================================================== =
 #  Xgboost model building ----
 # =============================================================================== =
-xgb_params = list(
-  objective = "binary:logistic",
-  eta = 0.01,
-  max.depth = 5,
-  eval_metric = "auc")
-
-
 date()
-xgb_fit <- xgboost(data = train_matrix, params = xgb_params, nrounds = 10000, print_every_n = 500)
+xgb_fit <- xgboost(data = train_matrix,
+                   max_depth = 4, eta = 0.5, nthread = 2, nrounds = 2000, print_every_n = 200, subsample = 0.5,
+                   objective = "multi:softprob", num_class = num_class)
+date()
 date()
 
 
@@ -138,59 +146,53 @@ date()
 # =============================================================================== =
 #  Making predictions from the model created and displaying a Confusion matrix ----
 # =============================================================================== =
-# This tutorial is very important to understant this part:
-# https://xgboost.readthedocs.io/en/latest/R-package/xgboostPresentation.html
 
 # ---------------------------------------------------------------- -
 #   * Using the train data ----
 # ---------------------------------------------------------------- -
-# Create our prediction probabilities
+
+# Predict for softmax returns num_class probability numbers per case:
 pred <- predict(xgb_fit, dtm_train)
 
-# Set our cutoff 
-pred.resp <- ifelse(pred >= 0.5, 1, 0)
 
-# Create the confusion matrix
-# https://rpubs.com/mharris/multiclass_xgboost
-confusionMatrix(factor(pred.resp), factor(labels_train), positive="1")
+# Reshape it to a num_class-columns matrix
+pred <- matrix(pred, ncol=num_class, byrow=TRUE)
+
+
+# Convert the probabilities to softmax labels
+pred_labels <- max.col(pred) - 1
+
+
+# Error: It should result in the same error as seen in the last iteration
+sum(pred_labels != labels_train)/length(labels_train)
+
+
+# Confusion matrix
+confusionMatrix(factor(pred_labels), factor(labels_train), positive="1")
 
 
 # ---------------------------------------------------------------- -
 #   * Using the test data ----
 # ---------------------------------------------------------------- -
-# Create our prediction probabilities
+
+# Predict for softmax returns num_class probability numbers per case:
 pred <- predict(xgb_fit, dtm_test)
 
-# Set our cutoff 
-pred.resp <- ifelse(pred >= 0.5, 1, 0)
 
-# Create the confusion matrix
-# https://rpubs.com/mharris/multiclass_xgboost
-confusionMatrix(factor(pred.resp), factor(labels_test), positive="1")
+# Reshape it to a num_class-columns matrix
+pred <- matrix(pred, ncol=num_class, byrow=TRUE)
 
 
+# Convert the probabilities to softmax labels
+pred_labels <- max.col(pred) - 1
 
 
-# =============================================================================== =
-#  Check the feature importance ----
-# =============================================================================== =
-importance_vars_train <- xgb.importance(model=xgb_fit, feature_names = colnames(train_matrix))
-head(importance_vars_train, 20)
+# Error: It should result in the same error as seen in the last iteration
+sum(pred_labels != labels_test)/length(labels_test)
 
 
+# Confusion matrix
+confusionMatrix(factor(pred_labels), factor(labels_test), positive="1")
 
-
-# =============================================================================== =
-#  Try to plot a partial dependency plot of one of the features ----
-# =============================================================================== =
-# partial(xgb_fit, train = text, pred.var = "bad")
-xgb.plot.importance(importance_vars_train, top_n = 20)
-xgb.plot.importance(importance_vars_train, top_n = sum(importance_vars_train$Gain >= 0.05))
-xgb.plot.importance(importance_vars_train, top_n = sum(cumsum(importance_vars_train$Gain) <= 0.85))
-
-
-xgb.plot.importance(importance_vars_test, top_n = 20)
-xgb.plot.importance(importance_vars_test, top_n = sum(importance_vars_test$Gain >= 0.05))
-xgb.plot.importance(importance_vars_test, top_n = sum(cumsum(importance_vars_test$Gain) <= 0.85))
 
 
